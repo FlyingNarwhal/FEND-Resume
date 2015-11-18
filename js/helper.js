9769e08702bdb5423f7c045c0a8da45e546ac050
Joe Dahle <dev.joe.dahle@gmail.com>
2015-11-18 11:21:23 -0700

    initial commit

--- js/helper.js
+++ js/helper.js
@@ -64,8 +64,16 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName(name) || function(){};
-    $('#name').html(iName);  
+      function inName() {
+        nameArray = bio.name.trim().split(" ");
+        console.log(name);
+        nameArray[0] = nameArray[0].slice(0,1).toUpperCase() +
+        nameArray[0].slice(1).toLowerCase();
+        nameArray[1] = nameArray[1].toUpperCase();
+        name = nameArray[0] + " " + nameArray[1];
+        return name;
+      }
+$('#name').html(inName);
   });
 });
 
@@ -85,7 +93,11 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  var x, y;
+  x = loc.pageX;
+  y = loc.pageY;
+
+  console.log(x, y);
 });
 
 
@@ -149,8 +161,8 @@ function initializeMap() {
   function createMapMarker(placeData) {
 
     // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.k;  // latitude from the place service
-    var lon = placeData.geometry.location.D;  // longitude from the place service
+    var lat = placeData.geometry.location.lat();  // latitude from the place service
+    var lon = placeData.geometry.location.lng();  // longitude from the place service
     var name = placeData.formatted_address;   // name of the place from the place service
     var bounds = window.mapBounds;            // current boundaries of the map window
 
@@ -170,7 +182,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -233,11 +245,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,30 +8,98 @@ var bio = {
 		"twitter": "@FlyingNarwhal",
 		"location": "Phoenix, Arizona"
 	},
-	"welcomeMessage": "string to change",
+	"welcomeMessage": "Resume for me, and stuff",
 	"skills": ["HTML", " CSS", " JavaScript"],
-	"bioPic": "https://s.gravatar.com/avatar/b578acdb4e61cde3df772c53b95394e9?s=80",
-	"display": "function"
-};
+	"bioPic": "images/fry.jpg",
+	"display": function() {
+		// variables for header
+		var formattedName    	  = HTMLheaderName.replace("%data%", bio.name);
+		var formattedRole    	  = HTMLheaderRole.replace("%data%", bio.role);
+		var formattedPic     	  = HTMLbioPic.replace("%data%", bio.bioPic);
+		var formattedMobile  	  = HTMLmobile.replace("%data%", bio.contacts.mobile);
+		var formattedEmail   	  = HTMLemail.replace("%data%", bio.contacts.email);
+		var formattedGit     	  = HTMLgithub.replace("%data%", bio.contacts.github);
+		var formattedTwitter 	  = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+		var formattedLocation	  = HTMLlocation.replace("%data%", bio.contacts.location);
+		var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+		var formattedSkills     = HTMLskills.replace("%data%", bio.skills);
+
+		// append header variables to page
+		$("#header").prepend(formattedRole);
+		$("#header").prepend(formattedName);
+		$("#topContacts").append(formattedMobile);
+		$("#topContacts").append(formattedEmail);
+		$("#topContacts").append(formattedGit);
+		$("#topContacts").append(formattedTwitter);
+		$("#topContacts").append(formattedLocation);
+		$("#header").append(formattedWelcomeMsg);
+		$("#header").append(formattedPic);
+		$("#header").append(HTMLskillsStart);	
+		for(skill in bio.skills){
+			formattedSkills = HTMLskills.replace("%data%", bio.skills[skill]);
+			$("#header").append(formattedSkills);
+		}
+	}
+}
 
 var education = {
 	"schools": [],
 	"onlineCourses": [{
 		"title": "Front-End Web Developer Nanodegree",
 		"school": "Udacity",
-		"date": 12312015,
+		"date": "December 31st 2015",
 		"url": "https://udacity.com"
 	}],
-	"display": "function"
-};
+	"display": function() {
+		for(course in education.onlineCourses){
+			//variables for online courses
+			var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+			var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+			var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].date);
+			var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+
+			//append online course HTML
+			$("#education").append(HTMLschoolStart)
+			$(".education-entry").append(formattedOnlineTitle);
+			$(".education-entry").append(formattedOnlineSchool);
+			$(".education-entry").append(formattedOnlineDates);
+			$(".education-entry").append(formattedOnlineURL);
+		}
+	}
+}
 
 var work = {
-	"jobs": {
+	"jobs": [{
 		"employer": "Florence Unified School District",
 		"title": "Bus Driver",
 		"location": "San Tan Valley, Arizona",
-		"dates": "May 2015-Current",
-		"description": "I drive kids all over the town."
+		"dates": "May 2015 - Currently",
+		"description": "I drive kids all over town."
+	},
+	{
+		"employer": "Kroger",
+		"title": "Day Stock",
+		"location": "San Tan Valley, Arizona",
+		"dates": "May 2011 - March 2013",
+		"description": "I put stuff on shelves and stuff."
+	}],
+	"display": function() {
+		$("#workExperience").append(HTMLworkStart);
+		for(job in work.jobs){
+			//variables for work
+			var formattedEmployer        = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+			var formattedTitle           = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+			var formattedWorkLocation    = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+			var formattedWorkDate        = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+			var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+			// append work values
+			$(".work-entry").append(formattedEmployer);
+			$(".work-entry").append(formattedTitle);
+			$(".work-entry").append(formattedWorkLocation);
+			$(".work-entry").append(formattedWorkDate);
+			$(".work-entry").append(formattedWorkDescription);
+		}
 	}
 };
 
@@ -40,48 +108,35 @@ var projects = {
 		"title": "Sous Chef",
 		"dates": "Currently developing",
 		"description": "Stop wondering what's for dinner, let's start cooking instead.",
-		"images": ["url for images"]
-	},{
-		"title": "Portfolio",
-		"dates": "October 2015",
-		"description": "See my projects",
-		"images": ["url for images"]
+		"images": ["file:///home/joe/Dropbox/udacity/resume/images/souschef.png"]
 	}],
-	"display": "function"
+	"display": function() {
+		$("#projects").append(HTMLprojectStart);
+		for(project in projects.projects){
+			var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+			var formattedProjectDate = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+			var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+			var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images);
+			
+			$(".project-entry").append(formattedProjectTitle);
+			$(".project-entry").append(formattedProjectDate);
+			$(".project-entry").append(formattedProjectDescription);
+			$(".project-entry").append(formattedProjectImage);
+		}
+
+	}
 };
 
-// variables for header
-var formattedName     = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole     = HTMLheaderRole.replace("%data%", bio.role);
-var formattedPic      = HTMLbioPic.replace("%data%", bio.bioPic);
-var formattedMobile   = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedEmail    = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedGit      = HTMLgithub.replace("%data%", bio.contacts.github);
-var formattedTwitter  = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-var formattedSkills   = HTMLskills.replace("%data%", bio.skills);
+function inName(name) {
+	name = name.trim().split(" ");
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+	return name[0] + " " + name[1];
+}
 
-// append header variables to page
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
-$("#header").append(formattedPic);
-$("#header").append(formattedMobile);
-$("#header").append(formattedEmail);
-$("#header").append(formattedGit);
-$("#header").append(formattedTwitter);
-$("#header").append(formattedLocation);
-$("#header").append(formattedSkills);
-
-// variables for work
-var formattedEmployer        = HTMLworkEmployer.replace("%data%", work.jobs.employer);
-var formattedTitle           = HTMLworkTitle.replace("%data%", work.jobs.title);
-var formattedWorkLocation    = HTMLworkLocation.replace("%data%", work.jobs.location);
-var formattedWorkDate        = HTMLworkDates.replace("%data%", work.jobs.dates);
-var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs.description);
-
-// append work values
-$("#workExperience").append(formattedEmployer);
-$("#workExperience").append(formattedTitle);
-$("#workExperience").append(formattedWorkLocation);
-$("#workExperience").append(formattedWorkDate);
-$("#workExperience").append(formattedWorkDescription);
+work.display();
+bio.display();
+education.display();
+projects.display();
+$("#main").append(internationalizeButton);
+$("#mapDiv").append(googleMap);
